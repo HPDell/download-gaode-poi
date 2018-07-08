a86828292f6d4fa89863c6f6c3cff7b1cf46016f
Yigong Hu <huyg0180110559@outlook.com>
2018-07-08 16:30:55 +0800

    添加：多个Key循环调用功能

--- index.ts
+++ index.ts
@@ -1,38 +1,48 @@
-import * as fs from "fs";
+import * as fs from "fs-extra";
 import * as json2csv from "json2csv";
 import { GaodePoi } from "./model";
 import { getGaodePoiData } from "./download";
+import { DownloadGaodeTarget } from "./targets";
+import { GaodeApiKey } from "./gaodeconfig";
 
-var CONFIG_FILE_PAHT = "gaodeconfig.json"
-
-async function main(outputroot: string, types: Array<{name:string, id: string}>) {
+export default async function downloadGaodePoi(config: GaodeApiKey[], targets: DownloadGaodeTarget[], outputroot: string) {
     // 读取ak
-    try {
-        var config:{key:string} = JSON.parse((await new Promise<Buffer>((resolve, rejection) => {
-            fs.readFile(CONFIG_FILE_PAHT, (err, data) => { if (err) rejection(err); else resolve(data); })
-        })).toString());
-        if (config.key && config.key.length > 0) {
-            // 爬取数据
-            var fields = GaodePoi.getFields();
-            for (var index = 0; index < types.length; index++) {
-                var poitype = types[index];
-                var poiList = await getGaodePoiData(config.key, new Array<string>(), [poitype.id], 20);
-                var poiCsv = json2csv({
-                    data: poiList,
-                    fields: fields
-                })
-                fs.writeFile(`${outputroot}/${poitype.name}.csv`, poiCsv, function (err: NodeJS.ErrnoException) {
-                    if (err) {
-                        console.log(err)
-                    } else {
-                        console.log(`${poitype.name} Saved!`)
-                    }
-                })
+    if (config.length > 0) {
+        // 爬取数据
+        for (const target of targets) {
+            var targetCity = target.city;
+            for (const targetType of target.types) {
+                // var poiList = await getGaodePoiData(config, targetCity, [], targetType.id, 20, 100);
+                try {
+                    var poiList = await getGaodePoiData(config, {
+                        city: targetCity,
+                        types: [targetType.id],
+                        offset: 20
+                    }, 100);
+                    saveToCsv(poiList, targetCity, targetType.name, outputroot)
+                } catch (error) {
+                    console.error(error);
+                }
             }
         }
-    } catch (error) {
+    } else {
         throw "No config file";
     }
 }
 
-main("./results", JSON.parse(fs.readFileSync("targets.json").toString()))
+function saveToCsv(poiList: GaodePoi[], city: string, type: string, outputroot: string) {
+    var poiCsv = json2csv({
+        data: poiList,
+        fields: GaodePoi.getFields()
+    })
+    var outputFile = `${outputroot}/${city}/${type}.csv`;
+    fs.ensureFile(outputFile, (err) => {
+        if (err) console.error(err);
+        else {
+            fs.writeFile(outputFile, poiCsv, function (err: NodeJS.ErrnoException) {
+                if (err) console.log(err)
+                else console.log(`${city}/${type} Saved!`)
+            })
+        }
+    })
+}
